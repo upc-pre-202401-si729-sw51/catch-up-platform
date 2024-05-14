45c9561fd3304e31a56c8f07f1e39da423bd2e94
Angel Velasquez <pcisavel@upc.edu.pe>
2024-05-14 09:52:34 -0500

    feat(inbound-services): added inbound service create action.

---
+++ src/main/java/com/acme/catchup/platform/news/interfaces/rest/FavoriteSourcesController.java
@@ -0,0 +1,39 @@
+package com.acme.catchup.platform.news.interfaces.rest;
+
+import com.acme.catchup.platform.news.domain.model.aggregates.FavoriteSource;
+import com.acme.catchup.platform.news.domain.services.FavoriteSourceCommandService;
+import com.acme.catchup.platform.news.domain.services.FavoriteSourceQueryService;
+import com.acme.catchup.platform.news.interfaces.rest.resources.CreateFavoriteSourceResource;
+import com.acme.catchup.platform.news.interfaces.rest.resources.FavoriteSourceResource;
+import com.acme.catchup.platform.news.interfaces.rest.transform.CreateFavoriteSourceCommandFromResourceAssembler;
+import com.acme.catchup.platform.news.interfaces.rest.transform.FavoriteSourceResourceFromEntityAssembler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/v1/favorite-sources")
+public class FavoriteSourcesController {
+    private final FavoriteSourceCommandService favoriteSourceCommandService;
+    private final FavoriteSourceQueryService favoriteSourceQueryService;
+
+    public FavoriteSourcesController(FavoriteSourceCommandService favoriteSourceCommandService, FavoriteSourceQueryService favoriteSourceQueryService) {
+        this.favoriteSourceCommandService = favoriteSourceCommandService;
+        this.favoriteSourceQueryService = favoriteSourceQueryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FavoriteSourceResource> createFavoriteSource(@RequestBody CreateFavoriteSourceResource resource) {
+        Optional<FavoriteSource> favoriteSource = favoriteSourceCommandService
+                .handle(CreateFavoriteSourceCommandFromResourceAssembler.toCommandFromResource(resource));
+        return favoriteSource.map(source ->
+                new ResponseEntity<>(FavoriteSourceResourceFromEntityAssembler.toResourceFromEntity(source), CREATED))
+                .orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+}
